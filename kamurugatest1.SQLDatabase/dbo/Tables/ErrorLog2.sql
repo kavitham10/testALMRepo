ae5637e953479cecbf8aac16a925258c96c13d02
kavitham10 <65304807+kavitham10@users.noreply.github.com>
2026-09-16 11:50:58 -0700

    Update ErrorLog2.sql

--- kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog2.sql
+++ kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog2.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[ErrorLog2] (
     [ErrorMessage]   NVARCHAR (4000) NOT NULL,
-     [ErrorMessage1]   NVARCHAR (4000) NOT NULL
+     [ErrorMessage1]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage2]   NVARCHAR (4000) NOT NULL
 );
 
 
