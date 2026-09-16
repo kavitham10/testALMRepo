8f2207ecce0a088808fd56bdeaabd446df0c9c36
kavitham10 <65304807+kavitham10@users.noreply.github.com>
2026-09-16 13:58:04 -0700

    Add ErrorMessage4 column to ErrorLog2 table

--- kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog2.sql
+++ kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog2.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[ErrorLog2] (
     [ErrorMessage]   NVARCHAR (4000) NOT NULL,
      [ErrorMessage1]   NVARCHAR (4000) NOT NULL,
     [ErrorMessage2]   NVARCHAR (4000) NOT NULL,
-    [ErrorMessage3]   NVARCHAR (4000) NOT NULL
+    [ErrorMessage3]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage4]   NVARCHAR (4000) NOT NULL
 );
 
 
