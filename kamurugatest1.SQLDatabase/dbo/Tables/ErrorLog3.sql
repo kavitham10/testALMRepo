3db8caf53ac72386a0e9658de375c37238c18d23
kavitham10 <65304807+kavitham10@users.noreply.github.com>
2026-09-18 14:14:14 -0700

    Create ErrorLog3.sql

---
+++ kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog3.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[ErrorLog2] (
+    [ErrorMessage]   NVARCHAR (4000) NOT NULL,
+     [ErrorMessage1]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage2]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage3]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage4]   NVARCHAR (4000) NOT NULL,
+     [ErrorMessage5]   NVARCHAR (4000) NOT NULL
+);
+
+
+GO
