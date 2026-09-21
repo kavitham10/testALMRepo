f9eaba0a1e80f2845e0a1db805fc1710c13834e2
kavitham10 <65304807+kavitham10@users.noreply.github.com>
2026-09-20 22:25:13 -0700

    Duplicate ErrorMessage5 column in ErrorLog5.sql
    Added duplicate column definition for ErrorMessage5.

--- kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog5.sql
+++ kamurugatest1.SQLDatabase/dbo/Tables/ErrorLog5.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[ErrorLog5] (
     [ErrorMessage2]   NVARCHAR (4000) NOT NULL,
     [ErrorMessage3]   NVARCHAR (4000) NOT NULL,
     [ErrorMessage4]   NVARCHAR (4000) NOT NULL,
-     [ErrorMessage5]   NVARCHAR (4000) NOT NULL
+     [ErrorMessage5]   NVARCHAR (4000) NOT NULL,
+    [ErrorMessage5]   NVARCHAR (4000) NOT NULL
 );
 
 
